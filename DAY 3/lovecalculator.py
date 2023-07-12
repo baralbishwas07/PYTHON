937d9da455c9878f6c8a224b678263790f689935
Bishwas Baral <baralbishwas1@gmail.com>
2023-07-13 00:11:51 +0545

    code correction

--- DAY 3/lovecalculator.py
+++ DAY 3/lovecalculator.py
@@ -2,21 +2,23 @@ name1 = input("What is your name?\n")
 name2 = input("What is their name?\n")
 combined_name = str(name1)+str(name2)
 lower_case = combined_name.lower()
-t = combined_name.count('t')
-r = combined_name.count('r')
-u = combined_name.count('u')
-e = combined_name.count('e')
+t = lower_case.count('t')
+r = lower_case.count('r')
+u = lower_case.count('u')
+e = lower_case.count('e')
+
 
 true = t + r + u + e
 
-l = combined_name.count('l')
-o = combined_name.count('o')
-v = combined_name.count('v')
-e = combined_name.count('e')
+l = lower_case.count('l')
+o = lower_case.count('o')
+v = lower_case.count('v')
+e = lower_case.count('e')
 
 love = l + o + v + e
 
 true_love = str(true) + str(love)
+print(true_love)
 true_love = int(true_love)
 
 if true_love <10 or true_love>90:
